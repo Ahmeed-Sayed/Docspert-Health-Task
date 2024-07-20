4e665e0f55bcb32dd12d3e4ade2139835004087c
Ahmeed-Sayed <ahmeedsayed01@gmail.com>
2024-07-20 18:47:06 +0300

    feat: added 404 page

--- account_transfer/settings.py
+++ account_transfer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3*93sdr!9g%oqxixl=v5y#-gxccwadcrn^n8cfh-e!v6zssx^7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
